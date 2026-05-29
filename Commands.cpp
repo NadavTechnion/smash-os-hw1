9df768dc82948ac387edfc231676094719888f80
BenShuster <ben.Shuster@campus.technion.ac.il>
2026-05-29 16:18:59 +0300

    added jobs implementation and external commands execute

--- Commands.cpp
+++ Commands.cpp
@@ -6,7 +6,7 @@
 #include <sys/wait.h>
 #include <iomanip>
 #include "Commands.h"
-
+#include <cctype>
 using namespace std;
 
 const std::string WHITESPACE = " \n\r\t\f\v";
@@ -114,29 +114,74 @@ Command *SmallShell::CreateCommand(const char *cmd_line) {
 
     return nullptr;
 }
-Command::Command(const char *cmd_line) : cmd_line(cmd_line) {}
+
+Command::Command(const char *cmd_line) : cmd_line(_rtrim(std::string(cmd_line))) {}
 Command::~Command() {}
 
 BuiltInCommand::BuiltInCommand(const char *cmd_line) : Command(cmd_line) {}
+
+ExternalCommand::ExternalCommand(const char *cmd_line, int p_id) : Command(cmd_line), p_id(p_id) {}
+
+void ExternalCommand::execute() {
+
+}
+
+/*
+void ExternalCommand::execute() {
+    bool is_bg = _isBackgroundComamnd(cmd_line.c_str());
+    bool is_complex = cmd_line.find('*') != string::npos || cmd_line.find('?') != string::npos;
+
+    char cmd_copy[COMMAND_MAX_LENGTH + 1];
+    strncpy(cmd_copy, cmd_line.c_str(), COMMAND_MAX_LENGTH);
+    cmd_copy[COMMAND_MAX_LENGTH] = '\0';
+    _removeBackgroundSign(cmd_copy);
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("smash error: fork failed");
+        return;
+    }
+    if (pid == 0) {
+        // child
+        setpgrp();
+        if (is_complex) {
+            execl("/bin/bash", "bash", "-c", cmd_copy, NULL);
+        } else {
+            char* args[COMMAND_MAX_ARGS + 1];
+            _parseCommandLine(cmd_copy, args);
+            execvp(args[0], args);
+        }
+        perror("smash error: exec failed");
+        exit(1);
+    } else {
+        // parent
+        p_id = pid;
+        if (!is_bg) {
+            waitpid(pid, nullptr, 0);
+        }
+    }
+}
+*/
+
+JobsList::JobsList() {}
+JobsList::~JobsList() {}
 
 ChpromptCommand::ChpromptCommand(const char *cmd_line) : BuiltInCommand(cmd_line) {}
 
 
 void SmallShell::executeCommand(const char *cmd_line) {
-    // TODO: Add your implementation here
-    // for example:
-    // Command* cmd = CreateCommand(cmd_line);
-    // cmd->execute();
-    // Please note that you must fork smash process for some commands (e.g., external commands....)
-    
     Command* cmd = CreateCommand(cmd_line);
-
-    if (cmd != nullptr) {
-        cmd->execute();
-        delete cmd; 
+    if(cmd == nullptr) return;
+    cmd->execute();
+    if(dynamic_cast<ExternalCommand*>(cmd) != nullptr) {
+        if(_isBackgroundComamnd(cmd_line)) {
+            jobs.addJob(cmd);
+        }
     }
-
+    delete cmd;
 }
+
+
 ShowPidCommand::ShowPidCommand(const char *cmd_line) : BuiltInCommand(cmd_line) {}
 
 void ShowPidCommand::execute() {
@@ -200,6 +245,15 @@ void ChangeDirCommand::execute() {
     // Clean up parsed arguments
     for (int i = 0; i < num_args; i++) {
         free(args[i]);
+    }
+}
+void JobsList::addJob(Command *cmd, bool isStopped) {
+    if (ExternalCommand* ext = dynamic_cast<ExternalCommand*>(cmd)) {
+        int new_id = 1;
+        for (auto& j : jobs) {
+            if (j.getJobId() >= new_id) new_id = j.getJobId();
+        }
+        jobs.push_back(JobEntry(new_id+1, ext->getPid(), cmd->getCmdLine(), isStopped));
     }
 }
 
--- smash.cpp
+++ smash.cpp
@@ -13,7 +13,8 @@ int main(int argc, char *argv[]) {
 
     SmallShell &smash = SmallShell::getInstance();
     while (true) {
-        std::cout << smash.getName() <<"> ";
+        std::cout << smash.getName() << "> "  << std::flush;
+;
         std::string cmd_line;
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
